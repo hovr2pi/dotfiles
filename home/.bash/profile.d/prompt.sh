6b40141069b14f6743718e776ff54acdfbd281dc
Christopher Simmons <csim@hovr2pi.org>
2013-12-12 03:56:59 -0600

    updated prompt.sh to use new all in one jesus prompt

--- home/.bash/profile.d/prompt.sh
+++ home/.bash/profile.d/prompt.sh
@@ -1,6 +1,2 @@
-PS1="[\t][\u@\h:\w]\$ "
-export PS1
-
-source $HOME/.dotsplat/repos/dotfiles/home/prompt/bash-git-prompt/gitprompt.sh
 source $HOME/.dotsplat/repos/dotfiles/home/prompt/bash-svn-prompt.sh
-
+source $HOME/.dotsplat/repos/dotfiles/home/prompt/bash-git-prompt.sh

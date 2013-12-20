bfed97ddbfeb5a9eed5c65862929302b449f1862
Christopher Simmons <csim@hovr2pi.org>
2013-12-19 23:14:15 -0600

    removed original gitprompt and replaced it with something less obnoxious

--- home/.bash/profile.d/prompt.sh
+++ home/.bash/profile.d/prompt.sh
@@ -1,6 +1,2 @@
-PS1="[\t][\u@\h:\w]\$ "
-export PS1
-
-source $HOME/.dotsplat/repos/dotfiles/home/.bash/prompt/bash-git-prompt/gitprompt.sh
-source $HOME/.dotsplat/repos/dotfiles/home/.bash/prompt/bash-svn-prompt.sh
+source $HOME/.dotsplat/repos/dotfiles/home/.bash/prompt/git-svn-bash.sh
 
